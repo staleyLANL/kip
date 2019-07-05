e486305dcaeabf0153e6f20d280e041387979fef
Martin Staley <kipnflip@gmail.com>
2019-07-04 23:13:27 -0600

    More command-line arguments; general cleanup/improvements.

--- example/kip.cc
+++ example/kip.cc
@@ -2,14 +2,14 @@
 // c++
 #include <map>
 
-// kip
-#include "kip.h"
-
 // gl, tk
 extern "C" {
 #include "gl.h"
 #include "tk.h"
 }
+
+// kip
+#include "kip.h"
 
 // kip: types
 using real  = double;
@@ -23,23 +23,36 @@ kip::light <real      > light;
 kip::engine<real      > engine;
 kip::image <real,color> image;
 
-// window: current size, initial position
-// Note: the rendered-image size is initially set to [hv]window,
-// but may become smaller if the undersampling feature is used.
-size_t hwindow = 1200;
-size_t vwindow = 1200;
-const size_t hpos = 50;
-const size_t vpos = 0;
-
-// variables: misc
+// misc
 size_t downsize;
-bool print;
 
-// variables: from command-line arguments
+
+
+// -----------------------------------------------------------------------------
+// variables from command-line arguments
+// -----------------------------------------------------------------------------
+
 namespace vars {
-   int  nrender = 0;
-   bool timing  = false;
-   bool debug   = false;
+   // window: size
+   size_t hwindow = 1200;
+   size_t vwindow = 1200;
+
+   // window: position
+   size_t hpos = 50;
+   size_t vpos = 0;
+
+   // initial #renders prior to user control;
+   // useful for timing
+   int nrender = 0;
+
+   // if timing: no window; render, exit
+   bool timing = false;
+
+   // print mode (print various parameters)
+   bool print = false;
+
+   // debug mode (very chatty)
+   bool debug = false;
 }
 
 
@@ -86,7 +99,6 @@ void initialize()
 
    // misc
    downsize = 1;
-   print = false;
 }
 
 
@@ -97,7 +109,7 @@ void initialize()
 // -----------------------------------------------------------------------------
 
 // render
-inline void render()
+void render()
 {
    if (vars::debug) {
       std::cout << "   render" << std::endl;
@@ -114,7 +126,7 @@ inline void render()
       return;
 
    // print
-   if (print)
+   if (vars::print)
       std::cout
          << "\nhpixel   = " << image .hpixel
          << "\nvpixel   = " << image .vpixel
@@ -125,7 +137,7 @@ inline void render()
          << "\nphi      = " << view  .phi
          << "\nroll     = " << view  .roll
       // << "\naspect   = " << image .aspect
-         << "\ndownsize = " <<        downsize
+         << "\ndownsize = " << downsize
          << "\nanti     = " << image .anti
       // << "\nlean     = " << engine.lean
          << "\n"
@@ -154,13 +166,13 @@ void putimage()
    }
 
    // size: same as window
-   if (image.hpixel == hwindow &&
-       image.vpixel == vwindow
+   if (image.hpixel == vars::hwindow &&
+       image.vpixel == vars::vwindow
    ) {
       if (vars::debug)
          std::cout << "      glDrawPixels #1" << std::endl;
       glDrawPixels(
-         GLsizei(hwindow), GLsizei(vwindow),
+         GLsizei(vars::hwindow), GLsizei(vars::vwindow),
          GL_RGBA, GL_UNSIGNED_BYTE, image()
       );
       glFlush();
@@ -169,23 +181,23 @@ void putimage()
 
    // size: different from window
    static kip::array<2,color> bitmap;
-   bitmap.upsize(hwindow,vwindow);
-   const real hfac = real(image.hpixel)/real(hwindow);
-   const real vfac = real(image.vpixel)/real(vwindow);
+   bitmap.upsize(vars::hwindow,vars::vwindow);
+   const real hfac = real(image.hpixel)/real(vars::hwindow);
+   const real vfac = real(image.vpixel)/real(vars::vwindow);
 
    #ifdef _OPENMP
    #pragma omp parallel for
    #endif
-   for (size_t n = 0;  n < hwindow*vwindow;  ++n)
+   for (size_t n = 0;  n < vars::hwindow*vars::vwindow;  ++n)
       bitmap[n] = image(
-         size_t(hfac*(real(n % hwindow) + 0.5)),
-         size_t(vfac*(real(n / hwindow) + 0.5))
+         size_t(hfac*(real(n % vars::hwindow) + 0.5)),
+         size_t(vfac*(real(n / vars::hwindow) + 0.5))
       );
 
    if (vars::debug)
       std::cout << "      glDrawPixels #2" << std::endl;
    glDrawPixels(
-      GLsizei(hwindow), GLsizei(vwindow),
+      GLsizei(vars::hwindow), GLsizei(vars::vwindow),
       GL_RGBA, GL_UNSIGNED_BYTE, bitmap.data()
    );
    glFlush();
@@ -216,8 +228,8 @@ bool sample(const sampling_t samp)
    // clip
    if (downsize < 1)
       downsize = 1;
-   if (downsize > std::min(hwindow,vwindow))
-      downsize = std::min(hwindow,vwindow);
+   if (downsize > std::min(vars::hwindow,vars::vwindow))
+      downsize = std::min(vars::hwindow,vars::vwindow);
 
    // different?
    return downsize != current_downsize;
@@ -279,7 +291,7 @@ bool move(const int key)
       case '.' : case '>' : return sample(sampling_finer);
 
       // toggle print, low(er)-memory mode
-      case XK_p : print = !print;  break;
+      case XK_p : vars::print = !vars::print;  break;
       //case XK_m : engine.lean = !engine.lean;  break;
 
       // home
@@ -301,10 +313,10 @@ bool move(const int key)
 // -----------------------------------------------------------------------------
 
 // diffsize: helper
-bool diffsize(size_t &himage, size_t &vimage)
+inline bool diffsize(size_t &himage, size_t &vimage)
 {
-   himage = std::max(size_t(1), size_t(real(hwindow)/real(downsize) + 0.5));
-   vimage = std::max(size_t(1), size_t(real(vwindow)/real(downsize) + 0.5));
+   himage = std::max(size_t(1), size_t(real(vars::hwindow)/real(downsize)+0.5));
+   vimage = std::max(size_t(1), size_t(real(vars::vwindow)/real(downsize)+0.5));
    return
       image.hpixel != himage ||
       image.vpixel != vimage;
@@ -317,16 +329,16 @@ void expose(const int hsize, const int vsize)
 {
    if (vars::debug) {
       std::cout << "EXPOSE" << std::endl;
-      std::cout << "   old hwindow = " << hwindow << std::endl;
-      std::cout << "   old vwindow = " << vwindow << std::endl;
+      std::cout << "   old hwindow = " << vars::hwindow << std::endl;
+      std::cout << "   old vwindow = " << vars::vwindow << std::endl;
    }
 
-   hwindow = size_t(hsize);
-   vwindow = size_t(vsize);
+   vars::hwindow = size_t(hsize);
+   vars::vwindow = size_t(vsize);
 
    if (vars::debug) {
-      std::cout << "   new hwindow = " << hwindow << std::endl;
-      std::cout << "   new vwindow = " << vwindow << std::endl;
+      std::cout << "   new hwindow = " << vars::hwindow << std::endl;
+      std::cout << "   new vwindow = " << vars::vwindow << std::endl;
    }
 
    size_t himage, vimage;
@@ -371,7 +383,10 @@ int interactive(const std::string &title)
 {
    if (!vars::timing) {
       // window: set position and size
-      tkInitPosition(int(hpos), int(vpos), int(hwindow), int(vwindow));
+      tkInitPosition(
+         int(vars::hpos), int(vars::vpos),
+         int(vars::hwindow), int(vars::vwindow)
+      );
 
       // window: open
       // Note: it seems that tkInitWindow() is not const correct...
@@ -425,10 +440,14 @@ int interactive(const std::string &title)
 
 // -----------------------------------------------------------------------------
 // command-line arguments
-// re: shapes
+// shapes
 // -----------------------------------------------------------------------------
 
 namespace args {
+
+// ------------------------
+// generic
+// ------------------------
 
 // shape
 template<class SHAPE>
@@ -438,14 +457,20 @@ inline bool shape(const int n)
    return true;
 }
 
+
+
+// ------------------------
+// specific
+// ------------------------
+
 // sphere
-static inline bool sphere(const int n)
+inline bool sphere(const int n)
 {
    return shape<kip::sphere<real,base>>(n);
 }
 
 // cone
-static inline bool cone(const int n)
+inline bool cone(const int n)
 {
    return shape<kip::cone<real,base>>(n);
 }
@@ -456,36 +481,81 @@ static inline bool cone(const int n)
 
 // -----------------------------------------------------------------------------
 // command-line arguments
-// re: misc
+// non-shapes
 // -----------------------------------------------------------------------------
 
 namespace args {
 
-// threads
-static inline bool threads(const int n)
+// ------------------------
+// window
+// ------------------------
+
+// hwindow
+inline bool hwindow(const int n)
 {
-   kip::threads = n;
+   vars::hwindow = std::max(100,std::abs(n));
    return true;
 }
 
+// vwindow
+inline bool vwindow(const int n)
+{
+   vars::vwindow = std::max(100,std::abs(n));
+   return true;
+}
+
+// hpos
+inline bool hpos(const int n)
+{
+   vars::hpos = std::max(0,std::abs(n));
+   return true;
+}
+
+// vpos
+inline bool vpos(const int n)
+{
+   vars::vpos = std::max(0,std::abs(n));
+   return true;
+}
+
+
+
+// ------------------------
+// misc, threads
+// ------------------------
+
 // render
-static inline bool render(const int n)
+inline bool render(const int n)
 {
    vars::nrender = std::abs(n);
    return true;
 }
 
 // timing
-static inline bool timing(const int)
+inline bool timing(const int)
 {
    vars::timing = true;
    return true;
 }
 
+// print
+inline bool print(const int)
+{
+   vars::print = true;
+   return true;
+}
+
 // debug
-static inline bool debug(const int)
+inline bool debug(const int)
 {
    vars::debug = true;
+   return true;
+}
+
+// threads
+inline bool threads(const int n)
+{
+   kip::threads = n;
    return true;
 }
 
@@ -500,20 +570,33 @@ static inline bool debug(const int)
 namespace args {
 
 // map
-static std::map<std::string, std::pair<bool (*)(const int), bool>> map = {
-   // last value == option has int parameter?
+// last value == option has int parameter?
+std::map<std::string, std::pair<bool (*)(const int), bool>> map = {
+   // shapes
    { "-sphere",  { args::sphere,  true  } },
    { "-cone",    { args::cone,    true  } },
-   { "-threads", { args::threads, true  } },
+   // fixme need lots more of these, and need to make them work
+
+   // window
+   { "-hwindow", { args::hwindow, true  } },
+   { "-vwindow", { args::vwindow, true  } },
+   { "-hpos",    { args::hpos,    true  } },
+   { "-vpos",    { args::vpos,    true  } },
+
+   // misc
    { "-render",  { args::render,  true  } },
    { "-timing",  { args::timing,  false } },
-   { "-debug",   { args::debug,   false } }
+   { "-print",   { args::print,   false } },
+   { "-debug",   { args::debug,   false } },
+
+   // threads
+   { "-threads", { args::threads, true  } },
 };
 
 
 
 // getint
-static inline bool getint(
+bool getint(
    const int argc, const char *const *const argv,
    const int i, int &n, std::string &title
 ) {
@@ -539,7 +622,7 @@ static inline bool getint(
 
 
 // read
-static bool read(
+bool read(
    const int argc, const char *const *const argv,
    std::string &title
 ) {
